4588529e669a2b85ab8c4bfbab6ae2b50f086bcc
loki6946 <aldianderson6@gmail.com>
2025-03-21 07:09:36 +0700

    make it use tagCreator

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import "./style.css"
+import tagsCreator from "./helpers/tags-creator"
 const root = document.querySelector("#root");
 
 function init(container) {
-  const helloWorld = document.createElement('h1');
-  helloWorld.textContent = "Hello World";
-  container.appendChild(helloWorld);
+  const sideBar = tagsCreator("div", "main__sidebar");
+  const mainTodos = tagsCreator("div", "main__todos");
+
+  container.appendChild(sideBar);
+  container.appendChild(mainTodos);
 }
 
 init(root)
